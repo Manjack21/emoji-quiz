364ce1d1110227c83fc68fd0beb2081d845e257f
Marco Winter <marco.winter@enviaM.de>
2023-07-21 21:41:25 +0200

    added category listing

--- backend/src/actions/listCategories.php
+++ backend/src/actions/listCategories.php
@@ -1,12 +1,27 @@
 
 <?php
-    $data = [];
-    $categories = $db->query("SELECT * FROM categories");
-    while ($res= $categories->fetchArray(1))
-    {
-        //insert row into array
-        array_push($data, $res);
+    // for url pattern /api.php/categories
+    if(count($route) == 1) {
+        $data = [];
+        $categories = $db->query("SELECT * FROM categories");
+        while ($res= $categories->fetchArray(1)) {
+            array_push($data, $res);
+        }
+        
+        echo json_encode($data);
+    }    
+    // for url pattern /api.php/categories/7/words
+    else if(count($route) == 3){
+        if(is_numeric($route[1]) and $route[2] == "words") {
+            $category_id = $route[1];
+        }
+        $categories = $db->query("SELECT w.* FROM categories c JOIN words w ON c.id = w.category_id WHERE c.id = '{$category_id}'");
+        $data = [];
+        while ($res= $categories->fetchArray(1)) {
+            array_push($data, $res);
+        }
+        
+        echo json_encode($data);
     }
-    
-    echo json_encode($data);
+
 ?>
