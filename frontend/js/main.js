399506cd3bf6c13c0b304717591d73c50753c697
Marco Winter <marco.winter@enviaM.de>
2023-07-22 14:00:09 +0200

    added back step function to input

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,12 +2,14 @@
 
 window.addEventListener("load", async function (ev) {
     const targetLabel = this.document.getElementById("current");
+    var history = [];
     function appendIcon(ev)
     {
-        const currentText = (targetLabel.dataset.current ?? "") + ev.target.id;
-        targetLabel.dataset.current = currentText;
+        const currentText = (targetLabel.dataset.current ?? "");
+        history.push(currentText);
 
-        targetLabel.innerHTML = currentText;
+        const newText = currentText + ev.target.id;
+        setCurrentText(newText);
         
         /*
         const glyphSpan = document.createElement("span");
@@ -15,17 +17,29 @@ window.addEventListener("load", async function (ev) {
         glyphSpan.addEventListener("click", function() { targetLabel.removeChild(glyphSpan); })
         
         targetLabel.appendChild(glyphSpan);
-        */
-        
-        document.getElementById("word").value = targetLabel.innerText.trim();
+        */       
         
     }
 
-    this.document.getElementById("clearButton").addEventListener("click",function(ev) {
-        targetLabel.innerHTML = "&nbsp;";
-        targetLabel.dataset.current = "";
-    });
+    this.document.getElementById("clearButton").addEventListener(
+        "click",
+        function(ev) { 
+            setCurrentText("&nbsp;")
+        }
+    );
 
+    this.document.getElementById("backButton").addEventListener(
+        "click",
+        function(ev) { 
+            if(history.length > 0) setCurrentText(history.pop());
+        }
+    );
+
+    function setCurrentText(text){
+        targetLabel.dataset.current = text;
+        targetLabel.innerHTML = (text === '' ? "&nbsp;" : text);
+        document.getElementById("word").value = targetLabel.innerText.trim();
+    }
     
     this.document.getElementById("category_id").innerHTML = await (await this.fetch("/api.php/categories")).text();
 
